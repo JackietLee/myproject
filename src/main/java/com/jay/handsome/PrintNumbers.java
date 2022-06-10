4c3369696860c5c0f570c31d501b696858ce203d
jay <lijian03@famsungroup.com>
2022-06-10 18:08:14 +0800

    剑指 Offer 17. 打印从1到最大的n位数

---
+++ src/main/java/com/jay/handsome/PrintNumbers.java
@@ -0,0 +1,35 @@
+package com.jay.handsome;
+
+import java.util.ArrayList;
+
+/**
+ * https://leetcode.cn/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/
+ *
+ * @author jay
+ * @date 2022/6/10 17:53
+ */
+public class PrintNumbers {
+    public int[] printNumbers(int n) {
+        ArrayList<Integer> arrayList = new ArrayList();
+        for (int i = 1;isLegal(i,n);i++){
+            arrayList.add(i);
+        }
+        int[] ints = new int[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++) {
+            ints[i] = arrayList.get(i);
+        }
+        return ints;
+    }
+
+    public boolean isLegal(int x, int n) {
+        double v = x % (Math.pow(10, n));
+        if (v == 0D){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean legal = new PrintNumbers().isLegal(1000, 3);
+    }
+}
